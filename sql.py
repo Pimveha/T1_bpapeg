563c7c05508db879aa809a0a087a6c3e881791aa
Melonkon <qridderpl@gmail.com>
2023-01-18 17:46:38 +0100

    Samenvoegen tot 1 python script

--- sql.py
+++ sql.py
@@ -1,53 +1,136 @@
 import psycopg2
+import datetime
 import requests
+import sys
+from Bio import Entrez
+from Bio import SeqIO
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+
+Entrez.email = "qridderpl@gmail.com"
 
 def database_maken():
-    # DB_NAME = "bpapget1_db"  # name database
-    # DB_USER = "bpapget1"  # name user
-    # DB_PASS = "bpapget1"  # password
-    # DB_HOST = "145.97.18.224"  # host
-    # DB_PORT = "8081"  # default
-    # connection = psycopg2.connect(database=DB_NAME, user=DB_USER,
-    #                               password=DB_PASS, host=DB_HOST, port=DB_PORT)
-
-    conn_string = """host='145.97.18.224' dbname='bpapget1_db' user='bpapget1' password='bpapget1'"""
+    conn_string = """host='145.97.18.224' dbname='s1136289_db' user='s1136289' password='s1136289'"""
     connection = psycopg2.connect(conn_string)
-
     print("Database maken en ermee verbinden is succesvol")
     return connection
 
 
-def ec_getter(connection, row):
+def extractor(genbank):
+    for info in genbank.splitlines():
+        if "LOCUS" in info:
+            eiwit_len = " ".join(info.split()[2:3])
+
+        if "/chromosome" in info:
+            chrom_num = info.strip().split("=")[1].strip("\"")
+
+        if '/db_xref=\"GeneID:' in info:
+            NCBI_gene_id = info.strip().split(":")[1].strip("\"")
+            NCBI_gene_id_found = True
+
+        if "/sex" in info:
+            sex_ = info.strip().split("=")[1].strip("\"")
+
+        if "/gene" in info:
+            gene_name = info.strip().split("=")[1].strip("\"")
+
+        if "/tissue_type" in info:
+            tissue_type = info.strip().split("=")[1].strip("\"")
+            tissue_type = tissue_type.replace(";", ",")
+
+        if "ACCESSION" in info:
+            protein_id = info.split()[1].strip("\"")
+        if "/product" in info:
+            protein = info.split("=")[1].strip().strip("\"")
+
+        if "/calculated_mol_wt" in info:
+            mol_wt = info.strip().split("=")[1].strip("\"")
+
+        if "/site_type" in info:
+            site_type = info.strip().split("=")[1].strip("\"")
+    with open("output.txt", "a") as output:
+        output.write(f"{NCBI_gene_id};{gene_name};{chrom_num};{protein_id};{protein};{eiwit_len};{sex_};{tissue_type}\n")
+
+
+def record_getter():
+    records_list = []
+    count = 0
+    with open("proteoom_ids.txt") as f:
+        f = f.readlines()
+        protein_ids = []
+        for x in f:
+            if "XP_" in x:
+                protein_ids.append(x.strip())
+                if len(protein_ids) > 9000:
+                    count += 1
+                    handle = Entrez.efetch(db="protein", id=",".join(protein_ids), rettype="gp")
+                    records_list.append(handle.read().split("//"))
+                    protein_ids = []
+                    print(f"{count}/{len(f)/9000}")
+                    break
+        handle = Entrez.efetch(db="protein", id=",".join(protein_ids), rettype="gp")
+        records_list.append(handle.read().split("//"))
+    return records_list
+
+
+def output_writer(records_list):
+    for records in records_list:
+        records.pop()
+        for bank in records:
+            try:
+                extractor(bank)
+            except:
+                continue
+
+
+def ec_getter(connection, row, gen_done):
     cur = connection.cursor()
-    content = requests.get(f"http://rest.genome.jp/get/oaa:{row[0]}")
-    for line in content.text.split("\n"):
-        if "[EC:" in line:
-            ecs = line.split("[")[1].split()
-            name = " ".join(line.split("[EC:")[0].split()[2:])
-            for ec in ecs:
-                # ec_lijst.append([ec.strip("EC:").strip("]"), row.split(';')[3]])
+    flag = False
+    pathway = ""
+    if row[0] not in gen_done:
+        content = requests.get(f"http://rest.genome.jp/get/oaa:{row[0]}")
+        for line in content.text.split("\n"):
+            if "MODULE" in line or "BRITE" in line:
+                flag = False
+                pathway = ""
+            if flag is True:
+                pathway = " ".join(line.split()[1:])
+            if "PATHWAY" in line:
+                flag = True
+                pathway = " ".join(line.split()[2:])
+            if len(pathway) > 2:
                 cur.execute("""
-                    INSERT INTO reactie ("ec_nummer", reactie_naam) 
-                    VALUES (%s, %s) ON CONFLICT (ec_nummer) DO NOTHING;
-                    """,
-                    (ec.strip("EC:").strip("]"), name))
-                
-                cur.execute("""
-                    INSERT INTO ec_nummer_eiwit_id ("ec_nummer", "eiwit_id") 
-                    VALUES (%s, %s) ;
-                    """,
-                    (ec.strip("EC:").strip("]"), row[3]))
-        if "NCBI-GeneID" in line:
-            print(line.split(":")[1].strip())
-        if "NCBI-ProteinID" in line:
-            print(line.split(":")[1].strip())
+                        INSERT INTO pathway_gen_id ("gen_id", pathway_name) 
+                        VALUES (%s, %s);
+                        """,
+                        (row[0], pathway)) 
+            if "[EC:" in line:
+                ecs = line.split("[EC:")[1].split()
+                name = " ".join(line.split("[EC:")[0].split()[2:])
+                for ec in ecs:
+                    # ec_lijst.append([ec.strip("EC:").strip("]"), row.split(';')[3]])
+                    cur.execute("""
+                        INSERT INTO reactie ("ec_nummer", reactie_naam) 
+                        VALUES (%s, %s) ON CONFLICT (ec_nummer) DO NOTHING;
+                        """,
+                        (ec.strip("EC:").strip("]"), name))
+                    cur.execute("""
+                        INSERT INTO ec_nummer_eiwit_id ("ec_nummer", "eiwit_id") 
+                        VALUES (%s, %s) ;
+                        """,
+                        (ec.strip("EC:").strip("]"), row[3]))
+    gen_done.add(row[0])                 
+    return gen_done
+
+
 
 def appender(connection):
     cur = connection.cursor()
+    gen_done = set()
     with open("output.txt", "r") as f:
-        for count ,row in enumerate(f.readlines(), 1):
+        f = f.readlines()
+        for count ,row in enumerate(f, 1):
             row = row.split(";")
-
             cur.execute("""
             INSERT INTO gen ("gen_id", "gen_name") 
             VALUES (%s, %s) ON CONFLICT (gen_id) DO NOTHING;
@@ -58,29 +141,82 @@ def appender(connection):
             INSERT INTO eiwit (eiwit_id, eiwit_naam, gen_id, chromosome, eiwit_lengte, sex_org, location_in_org) 
             VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (eiwit_id) DO NOTHING;
             """,
-            (row[3], row[4], row[0], row[2], row[5] ,row[6], row[7]))   
+            (row[3], row[4], row[0], row[2], row[5] ,row[6], row[7]))
+            gen_done = ec_getter(connection, row, gen_done)
+            print(f"{count}/{len(f)}")
+    connection.commit()
 
-            cur.execute("""
+
+def seq_appender(connection):
+    cur = connection.cursor()
+    sequentie_lijst = []
+    count = 0
+    with open("T1.fa") as sequenties, open("ensemble_ids.txt") as eiwit_id:
+        eiwit = eiwit_id.readlines()
+        for seq in sequenties:
+            if len(seq) > 60:
+                count += 1
+                cur.execute("""
             INSERT INTO mrna_brokstukken ("seq_id", "eiwit_id", sequentie_len ,sequentie) 
             VALUES (%s, %s, %s, %s) ON CONFLICT (seq_id) DO NOTHING;
             """,
-            (count, row[3], int(row[8]), row[9].strip()))   
+            (count, eiwit[count-1].split(".")[0], len(seq), seq))
 
-            ec_getter(connection, row)
 
-    connection.commit()
+def blast_seq():
+    with open("T1.fa", "r") as f:
+        f = f.readlines()
+        seq = f"{f[1]}"
 
-# def appender2(connection):
-#     cur = connection.cursor()
-#     with open("output.txt", "r") as f:
-#         for count ,row in enumerate(f.readlines()):
-                      
-#     connection.commit()
+    result_handle = NCBIWWW.qblast(program="blastn", database="nr" ,sequence=seq)
+    blast_records = NCBIXML.read(result_handle)
+    for count,x in enumerate(blast_records.descriptions):
+        if count == 1:
+            break
+        gi = x.title.split("|")[3]
+
+    handle = Entrez.efetch(db="nucleotide", id=f"{gi}", rettype="gp")
+    for x in handle.readlines():
+        if "ORGANISM" in x:
+            organisme = " ".join(x.split()[1:])
+            break
+    return organisme
+
+
+def assembly_getter(naam):
+    getal = 0
+    lijst = {}
+    handle = Entrez.esearch(db="assembly", term=f"{naam}[Organism]")
+    record = Entrez.read(handle)
+    organisme = record["IdList"]
+    for id in organisme:
+        esummary_handle = Entrez.esummary(db="assembly", id=id, report="full")
+        esummary_record = Entrez.read(esummary_handle)
+        if len(esummary_record['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_RefSeq']) > 0:
+            getal += 1
+            print(getal, esummary_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyAccession'], esummary_record['DocumentSummarySet']['DocumentSummary'][0]['Organism'], esummary_record['DocumentSummarySet']['DocumentSummary'][0]['SubmissionDate'], esummary_record['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_RefSeq'])
+            lijst[str(getal)] = esummary_record['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_RefSeq']
+
+    if len(lijst) == 0:
+        return False
+    keuze = input(f"Welke assembly wil je gebruiken? (Typ een getal tussen 1-{getal}) ")
+
+
+    if lijst.get(keuze)!=None:
+        output = f"{lijst[keuze]}/{lijst[keuze].split('/')[-1]}_protein.faa.gz"
+        with open("temp.txt", "w") as out:
+                out.write(output)
+        return True
+    else:
+        return False
+
+
 
 dropper = """
-    DROP TABLE IF EXISTS proteoom, eiwit, organism, gen, mrna_brokstukken, reactie ,ec_nummer_eiwit_id CASCADE;
+    DROP TABLE IF EXISTS proteoom, eiwit, organism, gen, mrna_brokstukken, reactie ,ec_nummer_eiwit_id, pathway_gen_id CASCADE;
     """
-    
+
+
 sql = """CREATE TABLE proteoom(
     proteoom_ID VARCHAR(50) PRIMARY KEY,
     lengte INT,
@@ -93,8 +229,8 @@ sql = """CREATE TABLE proteoom(
     eiwit_lengte INT,
     gen_id VARCHAR(50),
     ec_nummer VARCHAR(10),
-    chromosome INT,
-    reactie_id VARCHAR(50),
+    chromosome VARCHAR(10),
+    reactie_id VARCHAR(200),
     reactie_naam VARCHAR(50),
     proteoom_id VARCHAR(50) REFERENCES proteoom(proteoom_id),
     substrate VARCHAR(50),
@@ -128,23 +264,42 @@ sql = """CREATE TABLE proteoom(
     CREATE TABLE reactie(
     ec_nummer VARCHAR(50) PRIMARY KEY,
     reactie_id VARCHAR(50),
-    reactie_naam VARCHAR(50),
+    reactie_naam VARCHAR(200),
     substraat VARCHAR(50),
     product VARCHAR(50)
     );
     CREATE TABLE ec_nummer_eiwit_id(
     ec_nummer VARCHAR(50) REFERENCES reactie(ec_nummer),
     eiwit_id VARCHAR(20) REFERENCES eiwit(eiwit_id)
+    );
+    CREATE TABLE pathway_gen_id(
+    pathway_name VARCHAR(200),
+    gen_id VARCHAR(20) REFERENCES gen(gen_id)
     );"""
 
+
 def main():
-    
-    connection = database_maken()
-    cursor = connection.cursor()
-    cursor.execute(dropper)
-    cursor.execute(sql)
-    appender(connection)
-    connection.commit()
-    connection.close()
+    print(sys.argv[1])
+    if sys.argv[1] == "1":
+        flag = False
+        while flag == False:
+            naam = input("Van welk organisme wil je de assembly? Druk enter als je niet weet van welk organisme de brokstukken afkomstig van zijn. ")
+            if naam == "":
+                print("BLASTen van 1 van de sequenties om te kijken om welk organisme het gaat. ")
+                flag = assembly_getter(blast_seq())
+            else:
+                flag = assembly_getter(naam)
+    if sys.argv[1] == "2":
+        records_list = record_getter()
+        output_writer(records_list)
+        connection = database_maken()
+        cursor = connection.cursor()
+        cursor.execute(dropper)
+        cursor.execute(sql)
+        appender(connection)
+        seq_appender(connection)
+        connection.commit()
+        connection.close()
+
 
 main()
