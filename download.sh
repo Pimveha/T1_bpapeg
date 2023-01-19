5dda76421dd86517c2bbebd552d868b525dd92cc
Melonkon <qridderpl@gmail.com>
2023-01-19 23:35:43 +0100

    Kleine fout

--- download.sh
+++ download.sh
@@ -11,7 +11,7 @@ then
         mv *.faa OrnAna.fa
         makeblastdb -in OrnAna.fa -dbtype prot -parse_seqids
         blastx -query T1.fa -db OrnAna.fa -outfmt 6 -out total.txt
-        sort -k1,1 -k11,11g total.txt | sort --merge -u  -k1,1 > totaal_sort.txt
+        sort -k1,1 -k11,11g total.txt | sort --merge -u  -k1,1 > total_sort.txt
         cat total_sort.txt | awk '{print $2}' > ensemble_ids.txt
         grep -oE "^>\w+" OrnAna.fa | sed 's/>//g' >> proteoom_ids.txt
 
